2a2541921472ccb5def85507e7352cb77ab21097
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-03-05 12:10:27 +0600

    category wise card showing

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
+console.log("Hi from shahadat");
+
+const loadingSpinner = document.getElementById("loadingSpinner");
+
+const showLoading = () => {
+  loadingSpinner.classList.remove("hidden");
+  loadingSpinner.classList.add("flex");
+
+  treesContainer.innerHTML = "";
+};
+const hideLoading = () => {
+  loadingSpinner.classList.add("hidden");
+  loadingSpinner.classList.remove("flex");
+
+  //   treesContainer.innerHTML = "";
+};
+
 const loadCategories = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/categories",
@@ -17,6 +34,9 @@ const displayCategories = (categories) => {
     const cateBtn = document.createElement("button");
     cateBtn.className = "btn btn-soft btn-success w-full";
     cateBtn.textContent = category.category_name;
+    cateBtn.onclick = () => {
+      selectCategory(category.id, cateBtn);
+    };
     // cateBtn.innerHTML = `
 
     // <button id="cateTreesBtn" class="btn btn-soft btn-success w-full">
@@ -28,9 +48,37 @@ const displayCategories = (categories) => {
   });
 };
 
+const selectCategory = async (categoryId, btn) => {
+  console.log(categoryId, btn);
+  showLoading();
+
+  const allButtons = document.querySelectorAll(
+    "#categoriesContainer button,#allTreesBtn",
+  );
+
+  allButtons.forEach((btn) => {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-soft");
+  });
+
+  btn.classList.add("btn-success");
+  btn.classList.remove("btn-soft");
+
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/category/${categoryId}`,
+  );
+  const data = await res.json();
+  console.log(data);
+  displayTrees(data.plants);
+
+  hideLoading();
+};
+
 const loadTrees = async () => {
+  showLoading();
   const res = await fetch("https://openapi.programming-hero.com/api/plants");
   const data = await res.json();
+  hideLoading();
   displayTrees(data.plants);
 
   //   displayTrees(data);
@@ -64,7 +112,7 @@ const displayTrees = (trees) => {
                 <div class="badge badge-outline badge-success">${tree.category}</div>
 
                 <div class="card-actions justify-between items-center">
-                  <h2 class="font-bold text-xl text-success">${tree.price}</h2>
+                  <h2 class="font-bold text-xl text-success">$ ${tree.price}</h2>
                   <button class="btn btn-success">Buy Now</button>
                 </div>
               </div>
